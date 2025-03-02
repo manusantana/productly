2b10debad153d17533ea680ff873534a6ded07c2
Manu Santana <manu@manusantana.com>
2025-03-02 22:40:04 +0100

    actualizacion de settings a Debug false y freeze de archivo

--- productly/settings.py
+++ productly/settings.py
@@ -36,9 +36,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1kpyu12+4c^l1qhgz(knlt*gy8d7^4znj98&km1ee^li&(8=r9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
